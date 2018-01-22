be7f7b5c7c02bbf9747f9fce07c6198e6c0da084
Sublunarwind <kaidi.guo@outlook.com>
2018-01-22 13:07:50 +0100

    电波字幕组 字幕格式检查器2.1
    -修复句尾破折号时indexoutofrange异常
    -添加问题描述

---
+++ wave/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace wave
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Openfile = new System.Windows.Forms.Button();
+            this.Text1 = new System.Windows.Forms.TextBox();
+            this.Text2 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Openfile
+            // 
+            this.Openfile.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Openfile.Font = new System.Drawing.Font("Arial Narrow", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Openfile.Location = new System.Drawing.Point(366, 39);
+            this.Openfile.Name = "Openfile";
+            this.Openfile.Size = new System.Drawing.Size(189, 62);
+            this.Openfile.TabIndex = 0;
+            this.Openfile.Text = "选择字幕文件";
+            this.Openfile.UseVisualStyleBackColor = true;
+            this.Openfile.Click += new System.EventHandler(this.Open_click);
+            // 
+            // Text1
+            // 
+            this.Text1.Font = new System.Drawing.Font("SimSun", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Text1.Location = new System.Drawing.Point(25, 147);
+            this.Text1.Multiline = true;
+            this.Text1.Name = "Text1";
+            this.Text1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Text1.Size = new System.Drawing.Size(680, 680);
+            this.Text1.TabIndex = 1;
+            // 
+            // Text2
+            // 
+            this.Text2.Font = new System.Drawing.Font("SimSun", 7.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.Text2.Location = new System.Drawing.Point(765, 147);
+            this.Text2.Multiline = true;
+            this.Text2.Name = "Text2";
+            this.Text2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.Text2.Size = new System.Drawing.Size(680, 680);
+            this.Text2.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(25, 3);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(297, 129);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(125)))), ((int)(((byte)(227)))), ((int)(((byte)(227)))));
+            this.pictureBox2.Location = new System.Drawing.Point(25, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1420, 129);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.BackColor = System.Drawing.SystemColors.ControlText;
+            this.progressBar1.Location = new System.Drawing.Point(588, 67);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(401, 12);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1474, 859);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.Text2);
+            this.Controls.Add(this.Text1);
+            this.Controls.Add(this.Openfile);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(1500, 930);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(1500, 930);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "电波字幕组格式检查器2.0";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Openfile;
+        private System.Windows.Forms.TextBox Text1;
+        private System.Windows.Forms.TextBox Text2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ wave/Form1.cs
@@ -0,0 +1,308 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+
+/**电波字幕组，字幕格式检查器
+ * created by LSD猴子**/
+
+namespace wave
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public bool Linecheck(string thisline, int linenumber)
+        {
+            if (linenumber % 5 == 0)
+            {
+                //this is a blank line 5n
+                if (thisline.Length != 0)
+                {
+                    int srtline =1 + linenumber / 5; 
+                    string errormessage = "检查文本第" + linenumber + "行(字幕第"+srtline+"句)前是否缺行或多行";
+                    MessageBox.Show(errormessage);
+                    return false;
+                }
+                
+            }
+            else if (linenumber % 5 == 1)
+            {
+                //this is a number line 5n+1
+                int number;
+                bool result = Int32.TryParse(thisline, out number);
+                if (result)
+                {
+
+                }
+                else
+                {
+                    int srtline =1 + linenumber / 5;
+                    string errormessage = "检查文本第" + linenumber + "行(字幕第" + srtline + "句)前是否缺行或多行";
+                    MessageBox.Show(errormessage);
+                    return false;
+                }
+
+            }
+            return true;
+        }
+
+        public void Read1(string path)
+        {
+            System.IO.StreamReader sr = new System.IO.StreamReader(path, Encoding.Default);
+            String line;
+            int linenumber = 0;
+            bool linemiss = true;
+            while ((line = sr.ReadLine()) != null)
+            {
+                linenumber += 1;
+                if (Linecheck(line, linenumber) == false)
+                {
+                    linemiss = false;
+                    break;
+
+                }
+            }
+            if (linemiss)
+            {
+                Read2(path, linenumber);
+
+            }
+        }
+
+        public void Read2(string path, int totalline)
+        {
+            
+            
+                           
+            //初始化进度条
+            progressBar1.Visible = true;
+            progressBar1.Minimum = 1;
+            progressBar1.Maximum = totalline;
+            progressBar1.Step = 1;
+
+            System.IO.StreamReader sr = new System.IO.StreamReader(path, Encoding.Default);
+            String line;
+            List<string> wrongline = new List<string>();
+            List<string> modifiedline = new List<string>();
+            int linenumber = 0;
+            while ((line = sr.ReadLine()) != null)
+            {   
+                //计算进度条进度
+                progressBar1.PerformStep();
+
+                //Text1.Text = "read: " + line;
+                linenumber += 1;
+                Console.WriteLine("line=" + linenumber+ " "+line);
+
+                int length_line = line.Length;
+                string modified=line;
+                int count = 0;
+                
+                //在这里添加需要检查的符号
+                if (line.Contains('“'))//中文引号“” 英文引号""
+                {
+                    modified = modified.Replace('“', '"');
+                }
+                if (line.Contains('”'))
+                {
+                    modified = modified.Replace('”', '"');
+                }
+
+                try {
+                
+                for (int i = 0; i < length_line; i++)
+                {
+                    char this_character = line[i];
+                    if (this_character == '-'&& (linenumber - 2) % 5 != 0 && (linenumber - 1) % 5 != 0)
+                    {
+                        if (i==0)//如果-在句首的情况，只考虑其后
+                        {
+                            if(line[i+1]!= ' ')
+                            {
+                                modified = modified.Insert((i + 1 + count), " ") + "【少空格】";
+                                count += 1;
+                                Console.Write("句首，缺空格，在后加一个空格");
+                                Console.WriteLine("index="+i);
+                            }
+                            if (line[i + 2] == ' ')
+                            {
+                                modified = modified.Remove(i+1,1) + "【多空格】";
+                                count += -1;
+                                Console.Write("句首，多空格，在后删除一个空格");
+                                Console.WriteLine("index=" + i);
+                            }
+                        }
+                        else if(i==1 && line[0] == ' ')
+                        {
+                            modified = modified.Remove(0, 1) + "【多空格】";
+                            count -= 1;
+                            Console.Write("句首多一个空格，删除");
+                            Console.WriteLine("index=" + i);
+
+                        }
+                        else//不在句首
+                        {
+                            //中文行
+                            if ((linenumber - 3) % 5 == 0)
+                            {
+                                if (line[i - 1] != ' ' && line[i + 1] != ' ')
+                                {
+                                    break;
+                                }
+                                if (line[i - 1] != ' ')
+                                {
+                                    modified = modified.Insert((i + count), " ") + "【少空格】";
+                                    count += 1;
+                                    Console.Write("前1缺空格，前1加空格");
+                                    Console.WriteLine("index" + i);
+                                }
+                                if (line[i - 2] != ' ')
+                                {
+                                    modified = modified.Insert((i + count), " ") + "【少空格】";
+                                    count += 1;
+                                    Console.Write("前2缺空格，前2加空格");
+                                    Console.WriteLine("index" + i);
+                                }
+                                if (line[i + 1] != ' ')
+                                {
+
+                                    modified = modified.Insert((i + 1 + count), " ") + "【少空格】";
+                                    count += 1;
+                                    Console.Write("后1少空格，后1加空格");
+                                    Console.WriteLine("index" + i);
+                                }
+
+                            }
+                            else//英文行
+                            {
+                                if (line[i - 1]!= ' '&& line[i + 1] != ' ')
+                                {
+                                    break;
+                                }
+                                if (line[i - 1] != ' ')
+                                {
+                                    modified = modified.Insert((i + count), " ") + "【少空格】";
+                                    count += 1;
+                                    Console.Write("前面缺空格，前面加一个空格");
+                                    Console.WriteLine("index" + i);
+                                }
+                                if (line[i + 1] != ' ')
+                                {
+
+                                    modified = modified.Insert((i + 1 + count), " ") + "【少空格】";
+                                    count += 1;
+                                    Console.Write("后面少空格，后面加一个空格");
+                                    Console.WriteLine("index" + i);
+                                }
+                                if (line[i - 2] == ' ')
+                                {
+                                    modified = modified.Remove(i-1, 1) + "【多空格】";
+                                    count -= 1;
+                                    Console.Write("前1多空格，删除一个空格");
+                                    Console.WriteLine("index" + i+ "line"+linenumber);
+                                }
+                                if (line[i + 2] == ' ')
+                                {
+                                    modified = modified.Remove(i + 1, 1)+ "【多空格】";
+                                    count -= 1;
+                                    Console.Write("后1多空格，删除一个空格");
+                                    Console.WriteLine("index" + i + "line" + linenumber);
+                                }
+
+                            }
+
+                            
+
+                        }
+                    }
+                }
+                }
+                catch(System.IndexOutOfRangeException e)
+                {
+                    modified = "【检查此行是否有多余破折号】";
+                }
+
+
+
+                int srtnumber = 1 + linenumber / 5;
+                if (line!=modified) {
+                    wrongline.Add("文本第"+linenumber+ "行，字幕第"+srtnumber+"句:" + line);
+                    modifiedline.Add(modified);
+                }
+                
+            }
+            string combine_wrongline = string.Join("\r\n\r\n", wrongline.ToArray());
+            string combine_modifiedline = string.Join("\r\n\r\n", modifiedline.ToArray());
+            Text1.Text = combine_wrongline;
+            Text2.Text = combine_modifiedline;
+            
+            
+
+        }
+        
+        private void Open_click(object sender, EventArgs e)
+        {
+            //选择文件
+
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Multiselect = true;
+
+            //文件格式
+
+            openFileDialog.Filter = "所有文件|*.*";
+
+            //还原当前目录
+
+            openFileDialog.RestoreDirectory = true;
+
+            //默认的文件格式
+
+            openFileDialog.FilterIndex = 1;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+
+            {
+
+                string path = openFileDialog.FileName;
+                Read1(path);
+
+
+            }
+
+
+
+            ////选择文件夹
+
+            //FolderBrowserDialog dialog = new FolderBrowserDialog();
+
+            //dialog.Description = "请选择文件路径";
+
+            //if (dialog.ShowDialog() == DialogResult.OK)
+
+            //{
+
+            //    string foldPath = dialog.SelectedPath;
+
+            //    MessageBox.Show("已选择文件夹:" + foldPath, "选择文件夹提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            //}
+
+        }
+    }
+}
